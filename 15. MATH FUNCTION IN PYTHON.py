191bba216ad81cd6efb2d06af12bfab7af5ab5a9
KanwalSaeed <kanwal_saeed122@hotmail.com>
2020-02-06 19:49:44 +0500

    first code

---
+++ 15. MATH FUNCTION IN PYTHON.py
@@ -0,0 +1,23 @@
+
+number = 3.9
+print(round(number))
+
+marks = 266737.55
+print(round(marks))
+
+number = -6
+print(abs(number))
+
+print(min(6, 5.5))
+print(max(4.8, 4.9))
+print(pow(3, 2))
+print(pow(3,3 ))
+print(pow(280, 4))
+
+from math import*
+number = 25
+print(sqrt(25))
+number = 6.7
+print(ceil(number))
+number = 6.7
+print(floor(number))
